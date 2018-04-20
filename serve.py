d1c74166175837650dffcd79853d47acbbaa1ab3
Shubham Sakhuja <6405307+ssakhuja57@users.noreply.github.com>
2018-04-20 10:46:11 -0400

    Update serve.py

--- serve.py
+++ serve.py
@@ -9,7 +9,7 @@ import datetime
 class hostname:
 
   def GET(self):
-    return json.dumps({'msg': 'hi there from ' + socket.gethostname()})
+    return json.dumps({'msg': 'hello there from ' + socket.gethostname()})
 
 class env_vars:
 
